ee20f9d6334b172027670afc3559d05ade3c3123
manu.s <manu0501@icloud.com>
2023-02-01 13:04:55 +0100

    Added "menu"

--- MusiMathics/src/it/unisa/progetto/Fib.java
+++ MusiMathics/src/it/unisa/progetto/Fib.java
@@ -1,6 +1,8 @@
 package it.unisa.progetto;
 
+import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
+import java.util.Scanner;
 
 public class Fib {
 	static int SIZE = 16;
@@ -48,7 +50,7 @@ public class Fib {
 			fib[i] = fib[i - 2] + fib[i - 1];
 			/*
 			 * Si calcola il resto della divisione del valore ottenuto per 7.
-			 * Usiamo l'operatore modulo affinché si utilizzi solo i 7 tasti (ad esempio di
+			 * Usiamo l'operatore modulo affinchè si utilizzi solo i 7 tasti (ad esempio di
 			 * un pianoforte)
 			 * e armonizzare al meglio la melodia
 			 * 
@@ -57,7 +59,7 @@ public class Fib {
 			digits = digits + fib[i];
 		}
 
-		System.out.format("digits=%s\n", digits);
+		System.out.format("Stringa fibonacci = %s\n", digits);
 
 		String music = "";
 		String music1 = "";
@@ -87,7 +89,7 @@ public class Fib {
 			int index1 = Integer.parseInt("" + digits.charAt(x));
 			if (index1 == 0 || index1 == 6)
 				music1 += "h";
-			else
+			else if (!music1.equals(""))
 				music1 += "q";
 			music1 = music1 + " " + newMusic[index1];
 		}
@@ -98,7 +100,7 @@ public class Fib {
 			int index2 = Integer.parseInt("" + digits.charAt(x));
 			if (index2 == 3 || index2 == 6)
 				music2 += "h";
-			else
+			else if (!music2.equals(""))
 				music2 += "q";
 			music2 = music2 + " " + newMusic1[index2];
 
@@ -107,37 +109,83 @@ public class Fib {
 			newMusic2[j] = note[j].substring(0, 1) + octave;
 		for (int x = 0; x < digits.length(); x++) {
 			int index3 = Integer.parseInt("" + digits.charAt(x));
-			if (index3 == 1 || index3 == 6)
+			if (!music3.equals("") && (index3 == 1 || index3 == 6))
 				music3 += "h";
-			else
+			else if (!music3.equals(""))
 				music3 += "q";
 			music3 = music3 + " " + newMusic2[index3];
 
 		}
-
-		/* Stampa di tutte le note musicali prodotte */
-
-		System.out.format("music=%s\n", music);
-		System.out.format("music1=%s\n", music1);
-		System.out.format("music2=%s\n", music2);
-		System.out.format("music3=%s\n", music3);
-		/*
-		 * Viene creato un oggetto di classe Player con il nome playerMusic. La stringa
-		 * totaleMusic viene poi assegnata all'oggetto playerMusic per essere
-		 * riprodotta.
-		 * La stringa totaleMusic è una combinazione delle quattro stringhe musicali,
-		 * music, music1, music2 e music3,
-		 * che verranno riprodotte con diversi canali V0, V1, V2 e V3 con le relative
-		 * istruzioni per la riproduzione.
-		 * 
-		 */
-		Player playerMusic = new Player();
-		String totalMusic = "V0 I[FLUTE] T[Moderato] :CON(7, 70) " + music + " rw rw rw rw rw rw"
-				+ " V1 I[FLUTE] T[Moderato] :CON(7, 70) | rw rw rw " + music1 + " rw rw"
-				+ " V2 I[FLUTE] T[Moderato] :CON(7, 70) | rw " + music2 + " V3 I[FLUTE] T[Moderato] :CON(7, 70) | rw rw"
-				+ music3 + " T[Moderato] D6q E6q F6q G6q A6q B6q C7q D7q E7q F7q G7q A7q B7q C8w";
-		System.out.format("totalMusic = %s\n", totalMusic);
-		playerMusic.play(totalMusic);
+		int choice;
+		do {
+			System.out.println("1. Suona solo music");
+			System.out.println("2. Suona solo music1");
+			System.out.println("3. Suona solo music2");
+			System.out.println("4. Suona solo music3");
+			System.out.println("5. Suona simultaneamente");
+			System.out.println("6. Esci");
+			Scanner scanner = new Scanner(System.in);
+			choice = scanner.nextInt();
+			switch (choice) {
+				case 1:
+					Player playerMusic_0 = new Player();
+					Pattern music0 = new Pattern();
+					music0.add(" V0 I[FLUTE] T[Moderato] :CON(7, 70)" + music);
+					System.out.format("music=%s\n", music);
+					playerMusic_0.play(music0);
+					break;
+				case 2:
+					Player playerMusic_1 = new Player();
+					Pattern music_1 = new Pattern();
+					music_1.add(" V1 I[FLUTE] T[Moderato] :CON(7, 70)" + music1);
+					System.out.format("music1=%s\n", music1);
+					playerMusic_1.play(music_1);
+					break;
+				case 3:
+					Player playerMusic_2 = new Player();
+					Pattern music_2 = new Pattern();
+					music_2.add(" V2 I[FLUTE] T[Moderato] :CON(7, 70)" + music2);
+					System.out.format("music2=%s\n", music2);
+					playerMusic_2.play(music_2);
+					break;
+				case 4:
+					Player playerMusic_3 = new Player();
+					Pattern music_3 = new Pattern();
+					music_3.add(" V3 I[FLUTE] T[Moderato] :CON(7, 70)" + music3);
+					System.out.format("music3=%s\n", music3);
+					playerMusic_3.play(music_3);
+					break;
+				case 5:
+					/*
+					 * Viene creato un oggetto di classe Player con il nome playerMusic. La stringa
+					 * totaleMusic viene poi assegnata all'oggetto playerMusic per essere
+					 * riprodotta.
+					 * La stringa totaleMusic è una combinazione delle quattro stringhe musicali,
+					 * music, music1, music2 e music3,
+					 * che verranno riprodotte con diversi canali V0, V1, V2 e V3 con le relative
+					 * istruzioni per la riproduzione.
+					 * 
+					 */
+					Player playerMusic = new Player();
+					System.out.format("music=%s\n", music);
+					System.out.format("music1=%s\n", music1);
+					System.out.format("music2=%s\n", music2);
+					System.out.format("music3=%s\n", music3);
+					String totalMusic = "V0 I[FLUTE] T[Moderato] :CON(7, 70) " + music + " rw rw rw rw rw rw"
+							+ " V1 I[FLUTE] T[Moderato] :CON(7, 70) | rw rw rw " + music1 + " rw rw"
+							+ " V2 I[FLUTE] T[Moderato] :CON(7, 70) | rw " + music2
+							+ " V3 I[FLUTE] T[Moderato] :CON(7, 70) | rw rw"
+							+ music3 + " T[Moderato] D6q E6q F6q G6q A6q B6q C7q D7q E7q F7q G7q A7q B7q C8w";
+					System.out.format("totalMusic = %s\n", totalMusic);
+					playerMusic.play(totalMusic);
+					break;
+				case 6:
+					break;
+				default:
+					System.out.println("Scelta non valida");
+					break;
+			}
+		} while (choice < 6);
 	}
 
 }
